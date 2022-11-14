e48493ccd271d8e0ec6d7838dffef33299511b84
Vinit <vermavinit64@gmail.com>
2022-11-14 23:04:14 +0530

    circular progress bar complete

---
+++ script.js
@@ -0,0 +1,17 @@
+let progressBar = document.querySelector(".circular-progress");
+let valueContainer = document.querySelector(".value-container");
+
+let progressValue = 0;
+let progressEndValue = 100;
+let speed = 10;
+
+let progress = setInterval(() => {
+  progressValue++;
+  valueContainer.textContent = `${progressValue}%`;
+  progressBar.style.background = `conic-gradient(
+    #4d5bf9 ${progressValue * 3.6}deg, 
+    #cadcff ${progressValue * 3.6}deg)`;
+  if (progressValue == progressEndValue) {
+    clearInterval(progress);
+  }
+}, speed);
